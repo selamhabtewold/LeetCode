0317970f69c4b8748815d2d8aa1549f9b2ee676d
Selam Habtewold <125498535+selamhabtewold@users.noreply.github.com>
2024-07-02 11:30:49 +0530

    Time: 53 ms (61.75%), Space: 49.3 MB (39.3%) - LeetHub

---
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function(str1, str2) {
+    if (str1 + str2 !== str2 + str1) {
+        return "";
+    }
+
+    let gcdLength = gcd(str1.length, str2.length);
+    
+   
+    let gcdString = str1.substring(0, gcdLength);
+    
+    return gcdString;
+};
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
